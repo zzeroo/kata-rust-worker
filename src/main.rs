75a97ed5a05fb025808d88f82fcc40ac43d1b878
Stefan Müller zzeroo <co@zzeroo.com>
2020-09-29 13:21:45 +0200

    Async version with dropable worker

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,22 @@ use futures::sync::mpsc::{channel, Sender};
 use futures::{Future, Sink, Stream};
 
 fn main() {
-    let mut worker = spawn_worker();
+    let mut worker = Some(spawn_worker());
 
     let stdin = ::std::io::stdin();
     for line in stdin.lock().lines() {
         let line = line.unwrap();
-        let msg = if line == "stop" {
-            Msg::Stop
-        } else {
-            Msg::Echo(line)
+        if line == "stop" {
+            drop(worker.take());
+            continue;
         };
-        worker = worker.send(msg).wait().unwrap();
+
+
+        if let Some(w) = worker {
+            worker = Some(w.send(Msg::Echo(line)).wait().unwrap())
+        } else {
+            println!("The worker has been stopped!");
+        }
     }
 
     println!("Bye!");
@@ -25,25 +30,19 @@ fn main() {
 
 enum Msg {
     Echo(String),
-    Stop,
 }
 
 fn spawn_worker() -> Sender<Msg> {
     let (tx, rx) = channel(1);
     thread::spawn(move || {
-        let _ = rx
-            .for_each(|msg| match msg {
-                Msg::Echo(msg) => {
-                    println!("{} <3", msg);
-                    Ok(())
-                }
-                Msg::Stop => Err(()),
-            })
-            .then(|result| {
-                println!("The worker has stopped!");
-                result
-            })
-            .wait();
+        rx.for_each(|msg| {
+            match msg {
+                Msg::Echo(msg) => println!("{} <3", msg),
+            }
+            Ok(())
+        }).map(|()| {
+            println!("The worker has stopped!");
+        }).wait().unwrap();
     });
     tx
 }
