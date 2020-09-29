294c60471e645d08e23e73ae6496b053f460a9b0
Stefan Müller zzeroo <co@zzeroo.com>
2020-09-29 09:35:04 +0200

    Basic worker example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io::BufRead;
+use std::sync::mpsc::{Sender, channel};
+use std::thread;
+
+
 fn main() {
-    println!("Hello, world!");
+    let worker = spawn_worker();
+
+    let stdin = ::std::io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line.unwrap();
+        worker.send(Msg::Echo(line))
+            .unwrap();
+    }
+
+    println!("Bye!");
+}
+
+enum Msg {
+    Echo(String),
+}
+
+fn spawn_worker() -> Sender<Msg> {
+    let (tx, rx) = channel();
+    thread::spawn(move || {
+        loop {
+            let msg = rx.recv().unwrap();
+            match msg {
+                Msg::Echo(msg) => println!("{} ❤️", msg),
+            }
+        }
+    });
+
+    tx
 }
