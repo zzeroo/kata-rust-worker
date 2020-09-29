94054931ce843bcb10ab31d77bcb7a0a1cf701d7
Stefan Müller zzeroo <co@zzeroo.com>
2020-09-29 09:35:04 +0200

    First stopable solution

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::io::BufRead;
-use std::sync::mpsc::{Sender, channel};
+use std::sync::mpsc::{channel, Sender};
 use std::thread;
-
 
 fn main() {
     let worker = spawn_worker();
@@ -9,8 +8,13 @@ fn main() {
     let stdin = ::std::io::stdin();
     for line in stdin.lock().lines() {
         let line = line.unwrap();
-        worker.send(Msg::Echo(line))
-            .unwrap();
+        let msg = if line == "stop" {
+            Msg::Stop
+        } else {
+            Msg::Echo(line)
+        };
+
+        worker.send(msg).unwrap();
     }
 
     println!("Bye!");
@@ -18,6 +22,7 @@ fn main() {
 
 enum Msg {
     Echo(String),
+    Stop,
 }
 
 fn spawn_worker() -> Sender<Msg> {
@@ -26,9 +31,11 @@ fn spawn_worker() -> Sender<Msg> {
         loop {
             let msg = rx.recv().unwrap();
             match msg {
-                Msg::Echo(msg) => println!("{} ❤️", msg),
+                Msg::Echo(msg) => println!("{} <3", msg),
+                Msg::Stop => break,
             }
         }
+        println!("The worker has stopped");
     });
 
     tx
